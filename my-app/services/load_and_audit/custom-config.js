b86996189d05221ff6ed63591ec330213c03985e
Sytem-dev <system-legit@gmail.com>
2025-09-17 22:28:20 +0500

    Updated fix

--- my-app/services/load_and_audit/custom-config.js
+++ my-app/services/load_and_audit/custom-config.js
@@ -13,6 +13,11 @@
  */
 
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
 
